4330ed009c10d893cb6283ce3c0e3c96f43dabf4
Chuan1993 <chuan_1993@126.com>
2017-11-08 20:53:20 +0800

    nothing

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,10 @@
-print 'helloWorld'
+import numpy as np
+class Perception(object):
+	def __int__(self,eta=0.01,n_iter=10):
+		self.eta=eta
+		self.n_iter=n_iter
+		pass
+	def fit(self,x,y):
+		self.w_=np.zeros(1+x.shape[1]);
+		self.error=[]
+		pass
